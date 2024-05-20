765f38fb60c6f5f048705815d647a751d127eaca
Caiogithub18 <caioqueirozsouza4@gmail.com>
2024-05-20 20:20:33 -0300

    ex01

---
+++ ex01.php
@@ -0,0 +1,24 @@
+<?php  
+
+	$nota = array (10);
+
+	$nota[0] = "Pedro";
+	$note[1] = "Ricardo";
+	$nota[2] = "Henrique";
+	$nota[3] = "Caio";
+	$nota[4] = "Lucas";
+	$nota[5] = "Ana";
+	$nota[6] = "Maurício";
+	$nota[7] = "Maria";
+	$nota[8] = "Anderson";
+	$nota[9] = "Hiago";
+
+	foreach ($nota as $valor) {
+		echo $valor . ' <br />';
+
+}
+
+?>
+
+
+	
